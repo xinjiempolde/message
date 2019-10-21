d6d9a76328aeaae2e7886a579323c26ea1a3540f
xinjiempolde <xinjiempolde@gmai.com>
2019-10-21 16:30:48 +0800

    添加30s后功能

--- main/controller/add_message.py
+++ main/controller/add_message.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from .. import app, db
 from ..models.message import Message
-from flask import render_template, flash, request, redirect, url_for, session
+from flask import flash, request, redirect, url_for, make_response
 from datetime import datetime
 
 
@@ -11,6 +11,13 @@ def submit():
     name = sub_form.get("name")
     message = sub_form.get("message")
     in_reg_data = sub_form.get('reg_data')
+    # 获取上次提交时间，两次提交间需要间隔30s
+    last_time = request.cookies.get('time')
+    if last_time is not None:
+        last_time = datetime.strptime(last_time, "%Y-%m-%d %H:%M:%S")
+        if (datetime.now() - last_time).seconds < 30:
+            flash("留言后，请30s后再来留言！")
+            return redirect(url_for('index'))
     reg_data = request.cookies.get('reg_data')
     # 防止爬虫post提交
     if in_reg_data != reg_data or in_reg_data is None or reg_data is None:
@@ -29,4 +36,6 @@ def submit():
         db.session.add(one_message)
         db.session.commit()
         flash("你成功地进行了留言^_^~~~")
-        return redirect(url_for('index'))
+        rsp = make_response(redirect(url_for('index')))
+        rsp.set_cookie('time', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        return rsp
