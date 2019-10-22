d2300bcae2922ad4c842108d411514e8eb6d44a1
xinjiempolde <xinjiempolde@gmai.com>
2019-10-22 08:56:07 +0800

    user-agent

--- main/views/index.py
+++ main/views/index.py
@@ -3,7 +3,7 @@
     :author: Xinji Zhou
     :last edittime: 2019-10-14
 """
-from flask import render_template, make_response
+from flask import render_template, make_response, request
 from main import app
 from ..models.message import Message
 from datetime import datetime
@@ -13,6 +13,8 @@ import string
 
 @app.route('/')
 def index():
+    if request.user_agent.browser != 'chrome' or request.user_agent.browser != 'firefox':
+        return render_template('erros/browser.html')
     # 时间最新的在前面
     messages = Message.query.order_by(Message.submit_time.desc()).all()
     reg_data = ''.join(random.sample(string.ascii_letters + string.digits, 60))
